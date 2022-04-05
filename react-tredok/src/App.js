07e5f312198c6f5a5b315f6986ac1941aef7f155
Gibran Abner Abud Alcalá <tredok@ciencias.unam.mx>
2022-04-05 03:28:37 -0500

    Corrected an error
    - Removed BrowserRouter and Link, since they were not used.

--- react-tredok/src/App.js
+++ react-tredok/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Supaheader from './components/header/supa-header.js'
 import LandingPage from './pages/landing.js'
