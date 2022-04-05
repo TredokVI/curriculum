2ac13bcaabf39996d533d1e4325dc1ca1ab3da87
Gibran Abner Abud Alcalá <tredok@ciencias.unam.mx>
2022-04-05 03:26:55 -0500

    Changed fonts and styling of landing page
    - Added fonts
    - Display flexed them to be displayed nicely

--- react-tredok/src/pages/landing.js
+++ react-tredok/src/pages/landing.js
@@ -1,6 +1,8 @@
+import '../components/styling/landing.css'
+
 export default function LandingPage(){
   return(
-    <div>
+    <div className="body">
       <h2> WELCOME TO MY LAIR! </h2>
       <p> Click the fishy to get to the landing page </p>
       <p> Or click the dog to get to Dog Walking Simulator :D </p>
