12e4105056dbcc596463d502c55989b5b98f038d
Gibran Abner Abud Alcalá <tredok@ciencias.unam.mx>
2022-04-04 19:12:09 -0500

    Created React app
    Basic React app working.

---
+++ main/react-tredok/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Dws from './dws/dws.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Welcome to Dog Walking Simulator! </p>
+      </header>
+
+      <Dws className="dws"/>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ main/react-tredok/src/dws/dws.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl:    "games/DWS/Build/DWS 1.1.1.loader.js",
+  dataUrl:      "games/DWS/Build/DWS 1.1.1.data.unityweb",
+  frameworkUrl: "games/DWS/Build/DWS 1.1.1.framework.js.unityweb",
+  codeUrl:      "games/DWS/Build/DWS 1.1.1.wasm.unityweb",
+});
+
+function Dws() {
+  return <Unity unityContext={unityContext}
+          style={{
+           height: "720px",
+           width: "1280px",
+           border: "2px solid black",
+           background: "grey",
+         }}/>;
+}
+
+export default Dws;
