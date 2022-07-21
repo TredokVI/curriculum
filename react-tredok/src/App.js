0e0224f79b85210ae43b9a6bc9b2c038a575e84b
Gibran Abner Abud Alcalá <tredok@ciencias.unam.mx>
2022-07-21 02:20:56 -0500

    Integrated a new Landing Page made with TeleportHQ. Neat, easy, painless

--- react-tredok/src/App.js
+++ react-tredok/src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import './components/styling/App.css'
 
-import Supaheader from './components/header/supa-header.js'
-import LandingPage from './pages/landing.js'
-import Dws from './dws/dws.js';
+import Supaheader from './components/header/supa-header';
+import LandingPage from './pages/landing';
+import Landing from './pages/LandingPage';
+import Dws from './dws/dws';
 
 
-function App() {
+export default function App() {
   return (
       <div >
-        <Supaheader />
         <main>
           <Routes>
-            <Route path="/" exact element={<LandingPage />} />
+            <Route path="/" exact element={<Landing />} />
             <Route path="/dws" element={<Dws className="dws"/>} />
           </Routes>
         </main>
       </div>
   );
 }
-
-export default App;
